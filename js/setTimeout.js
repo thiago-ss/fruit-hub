e08014ea4c8f44c153b3c06992faa4ad79f7f17d
Thiago <thiago2508ss@gmail.com>
2022-05-19 09:47:59 -0300

    Finishing Splash Screen

---
+++ js/setTimeout.js
@@ -0,0 +1,7 @@
+const splash = document.querySelector('.splash');
+
+document.addEventListener('DOMContentLoaded', (e) => {
+    setTimeout(() => {
+      location.href = "views/welcome.html"
+    }, 6000)
+})
